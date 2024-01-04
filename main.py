ff14751665e666bd35804580bf5f7c416ea125f4
mattplantz <118403738+mattplantz@users.noreply.github.com>
2024-01-04 10:46:15 -0600

    Update main.py
    session state stuff

--- main.py
+++ main.py
@@ -7,17 +7,21 @@ uploaded_files = st.file_uploader("Please upload your Chase CC Account Activity"
 if uploaded_files not in st.session_state:
     st.session_state.uploaded_files = uploaded_files
 if uploaded_files:
-    read_data = [pd.read_csv(file) for file in uploaded_files]
-    df = pd.concat(read_data)
-    # flip sign of amount
-    df['Amount'] = df['Amount'] * - 1
-    # separate payments and charges
-    charges = df[df['Type'] == 'Sale']
-    payments = df[df['Type'] == 'Payment']
-    if 'charges' not in st.session_state:
-        st.session_state.charges = charges
-    if 'payments' not in st.session_state:
-        st.session_state.payments = payments
+    if 'df' in st.session_state:
+        df = st.session_state.df
+    else:
+        read_data = [pd.read_csv(file) for file in uploaded_files]
+        df = pd.concat(read_data)
+        # flip sign of amount
+        df['Amount'] = df['Amount'] * - 1
+    
+        # separate payments and charges
+        charges = df[df['Type'] == 'Sale']
+        payments = df[df['Type'] == 'Payment']
+        if 'charges' not in st.session_state:
+            st.session_state.charges = charges
+        if 'payments' not in st.session_state:
+            st.session_state.payments = payments
     
 with st.sidebar:
     Flag_Charges = st.checkbox("Flag Specific Charges")
