ae66ea997e35857e56a009ebf577a330312eaaa0
mattplantz <118403738+mattplantz@users.noreply.github.com>
2024-01-09 10:06:20 -0600

    Update Homepage.py
    added upper function to text to find to match output from Chase

--- Homepage.py
+++ Homepage.py
@@ -28,7 +28,7 @@ if Flag_Charges:
     text_to_find = st.text_input("What text would you like to flag? Please separate multiple search terms with '|'", '')
     if text_to_find != '' and 'charges' in st.session_state:
         charges = st.session_state.charges
-        charges['Flag'] = charges['Description'].str.contains(text_to_find).astype(int)
+        charges['Flag'] = charges['Description'].str.contains(text_to_find.upper()).astype(int)
         st.session_state['charges'] = charges
         st.subheader("Flagged Instances")
         st.write("There are ", charges['Flag'].sum(), "instances of your flag")
